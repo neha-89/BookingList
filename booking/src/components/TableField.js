1862a6cff35f8fb4b8262d2f2d596dbc62f06f37
Neha Sontakke <wasnik.neha@gmail.com>
2022-07-18 17:37:22 +0530

    added the icons

--- booking/src/components/TableField.js
+++ booking/src/components/TableField.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const TableField = ({fieldName}) =>{
+const TableField = ({fieldName ,settingsField}) =>{
     return(
         <>
         <div className="tableField">
             {fieldName}
-        </div>
+            {settingsField && 
+            <>
+            <FontAwesomeIcon icon="fa-solid fa-pen-to-square" />
+            <i className="fa-solid fa-pen-to-square"></i>
+            <i class="fa-solid fa-trash-can"></i>
+            </>
+            }
+         </div>
         </>
     )
 }
